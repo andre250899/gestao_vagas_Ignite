d73174d1d6dc76d262f8cf018a5bf2b33b091136
amuniz.dev <amuniz.dev@gmail.com>
2024-04-13 23:25:01 -0300

    Aula 04 - Inserindo Spring Data JPA

--- src/main/java/com/dev/amuniz/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/dev/amuniz/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,6 +1,10 @@
 package com.dev.amuniz.gestao_vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -8,9 +12,17 @@ import java.util.UUID;
 public class CandidateEntity {
 
     private UUID id;
+
     private String name;
+
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaços.")
     private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido.")
     private String email;
+
+    @Length(min = 6, max = 100, message = "O campo [Senha] deve ter entre  6 e 100 caracteres.")
     private String password;
     private String description;
     private String curriculum;
--- src/main/java/com/dev/amuniz/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/dev/amuniz/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,6 +1,7 @@
 package com.dev.amuniz.gestao_vagas.modules.candidate.controllers;
 
 import com.dev.amuniz.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
 
-    @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    @PostMapping()
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato");
         System.out.println(candidateEntity.getEmail());
     }
