310012dd9be900ef80fb2be2aa22167b87e19ef9
amuniz.dev <amuniz.dev@gmail.com>
2024-04-15 23:34:01 -0300

    Aula 12 - Company Controller

--- src/main/java/com/dev/amuniz/gestao_vagas/modules/company/controllers/CompanyController.java
+++ src/main/java/com/dev/amuniz/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -1,10 +1,8 @@
-package com.dev.amuniz.gestao_vagas.modules.candidate.controllers;
+package com.dev.amuniz.gestao_vagas.modules.company.controllers;
 
 import com.dev.amuniz.gestao_vagas.exceptions.UserFoundException;
-import com.dev.amuniz.gestao_vagas.modules.candidate.CandidateEntity;
-import com.dev.amuniz.gestao_vagas.modules.candidate.CandidateRepository;
-import com.dev.amuniz.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
-import jakarta.validation.Valid;
+import com.dev.amuniz.gestao_vagas.modules.company.entities.CompanyEntity;
+import com.dev.amuniz.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,18 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/candidate")
-public class CandidateController {
+@RequestMapping("/company")
+public class CompanyController {
 
     @Autowired
-    private CreateCandidateUseCase createCandidateUseCase;
+    private CreateCompanyUseCase createCompanyUseCase;
 
     @PostMapping("/")
-    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    public ResponseEntity<Object> create(@RequestBody CompanyEntity companyEntity) {
         try {
-            var result = this.createCandidateUseCase.execute(candidateEntity);
+            var result = this.createCompanyUseCase.execute(companyEntity);
             return ResponseEntity.ok().body(result);
         } catch (Exception e) {
+            e.printStackTrace();
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
